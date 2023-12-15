8f387a84cf0bbacac0518b3b9b0d542f53cf402f
Makbulhussain Ka <makbulhussain.ka@fractal.ai>
2023-12-15 10:22:54 +0530

    cherrypick2 commit

--- test_makbul.py
+++ test_makbul.py
@@ -2,10 +2,6 @@ print('Hello World')
 
 print('Hello World commit 1')
 
-<<<<<<< HEAD
 print('Hello World commit 2')
 
 print('Hello World commit 3')
-=======
-print('Hello World commit 2')
->>>>>>> 2bb8e8d (second commit)
