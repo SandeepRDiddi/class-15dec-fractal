b56195a781e663929bb9e952d2a30834cccf97cc
Makbulhussain Ka <makbulhussain.ka@fractal.ai>
2023-12-15 10:09:00 +0530

    Cherry-picked

--- test_makbul.py
+++ test_makbul.py
@@ -2,6 +2,10 @@ print('Hello World')
 
 print('Hello World commit 1')
 
+<<<<<<< HEAD
 print('Hello World commit 2')
 
 print('Hello World commit 3')
+=======
+print('Hello World commit 2')
+>>>>>>> 2bb8e8d (second commit)
